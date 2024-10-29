71790715cbebcaa4d1cacfc947b4ed590da41865
KiimEviL <kimasia3000@gmail.com>
2024-10-29 23:19:23 +0700

    Добавьте файлы проекта.

---
+++ 241029_myBasket/241029_myBasket.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Fruit {
+private:
+	string fName;
+public:
+	Fruit(const string& name = "") :fName(name) {};
+	//Fruit(string& name) : fName(name) {};
+	string getName() const {
+		//string getName()  {
+		return fName;
+	}
+};
+
+class Basket {
+private:
+	vector<Fruit> fBasket;
+public:
+	void add(const Fruit& fName) {
+		//void add( Fruit& fName) {
+		fBasket.push_back(fName);
+	}
+	void check() {
+		cout << "items in basket: " << endl;
+		for (vector<Fruit>::iterator i = fBasket.begin();
+			i != fBasket.end(); i++) {
+			cout << i->getName() << endl;
+		}
+	}
+};
+
+int main() {
+	Fruit f("arbuz");
+	f.getName();
+
+	Basket fBasket;
+	fBasket.add(Fruit("dynia"));
+	fBasket.add(Fruit("cherry"));
+	fBasket.check();
+}
